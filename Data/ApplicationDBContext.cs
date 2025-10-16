c1a5138fb51d9b25a74d5e1a38359abe5cd36c27
IT-Entech <IT-support@en-technology.com>
2025-10-16 15:54:36 +0700

    Add project files.

---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectCRM.Models;
+
+namespace ProjectCRM.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        // Define your DbSets (tables) here
+        public DbSet<Customer> a_user { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectCRM.Models
+{
+    public class Customer
+    {
+        [Key]
+        [Column(TypeName = "char(13)")]
+        public string? staff_id { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(100)")]
+        public required string username { get; set; }
+
+        [Column(TypeName = "varchar(25)")]
+        public string? Role { get; set; }
+
+        [Column(TypeName = "char(1)")]
+        public string? active { get; set; }
+    }
+}
